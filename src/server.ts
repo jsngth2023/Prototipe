063216e6334dbb12d6a36f28c60674f95a970385
Jonas Sousa Neves <jonassn2013@gmail.com>
2024-02-17 18:44:57 -0300

    continue

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
+import express, { Application } from 'express';
+import http from 'http';
+import {Server} from 'socket.io';
+
 class App{
+   private app: Application
+   private http: http.Server
+   private io: Server
+
     constructor(){
-        this.app = express
+        this.app = express();
+        this.http = http.createServer(this.app);
+        this.io = new Server(this.http);
+        this.listenSocket();
     }
     listenServer(){
-
+        this.http.listen(3000, ()=> console.log('server is running'));
+    }
+    listenSocket(){
+        this.io.on('connection', (socket)=>{
+          console.log('use connected =>', socket.id);
+        })
     }
 }
 
-const app = new App()
+const app = new App();
 
-app.listenServer()
+app.listenServer();
+app.listenSocket();
